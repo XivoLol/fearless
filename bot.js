faca23104f0b8699eb72ecb57e9601c86a4963cc
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-10-27 12:36:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const devs = ["336614900329611264"];
-const adminprefix = ["-"];
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
 
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();
-  } else
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+client.on('ready', () => { //playing
+    client.user.setGame(`Welcome To Fèarless Server..`,'https://www.twitch.tv/faresgameryt');
+    client.user.setStatus('Online')
 });
 
 client.on('guildMemberAdd',m =>{
