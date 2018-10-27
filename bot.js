171b420898e3ed0bde138a444403ee893f565848
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-10-27 13:12:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,16 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => { //playing
-    client.user.setGame(`Welcome To Fèarless Server..`,'https://www.twitch.tv/faresgameryt');
+    client.user.setGame(`Welcome To Stuning Server..`,'https://www.twitch.tv/faresgameryt');
     client.user.setStatus('Online')
 });
 
-client.on('guildMemberAdd',m =>{
-//خـليه يبحث عن الروم بالاول ..
-m.guild.channels.find(c => c.name === "fearless").then(c => { //بـعدين
-    c.send("**Welcome To Fèarless Server..**")
-    //يـبعت الرسالة
-})
-});
+
 
 client.login(process.env.BOT_TOKEN);
