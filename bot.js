82735472dc5b90b789642ce451f97d1447824764
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-10-29 17:00:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = 'X';
-client.on('message', message => {
-        if (!message.content.startsWith(prefix)) return;
-        var args = message.content.split(' ').slice(1);
-        var argresult = args.join(' ');
-        if (message.author.id !== "336614900329611264") return;
-      
-      if (message.content.startsWith(prefix + 'setstream')) {
-        client.user.setGame(argresult, "https://www.twitch.tv/mrx-dev");
-           console.log('test' + argresult);
-          message.channel.sendMessage(`Streaming: **${argresult}`)
-      }
-      
-      if (message.content.startsWith(prefix + 'setname')) {
-        client.user.setUsername(argresult).then
-            message.channel.sendMessage(`Username Changed To **${argresult}**`)
-        return message.reply("You Can change the username 2 times per hour");
-      }
-      if (message.content.startsWith(prefix + 'setavatar')) {
-        client.user.setAvatar(argresult);
-         message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-      }
-      if (message.content.startsWith(prefix + 'setgame')) {
-        client.user.setGame(argresult)
-        message.channel.sendMessage(`Game Changed Successfully To **${argresult}**`);
-    
-    
-    }
-      });
+client.on('ready', function(){
+    var ms = 4000 ;
+    var setGame = [`Welcome To Stúning`,`Tags Seller`,`Nitro Seller`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);
+
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
