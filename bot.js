e553d72e8e4f0ac59af7f8b2360203341d9e76c6
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-11-22 20:06:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-
-client.on("ready", () => {
-    client.user.setPresence({
-      status: 'dnd',
-      game: { 
-         type: 0,
-         name: 'Surprise Mother Fucker :) ',
-         details: `I'm : dÁrY - Developer and Programmer`,
-         url: 'http://twitch.tv/Streammingg',
-         state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
-        application_id: '336614900329611264',
-         assets: {
-    
-      }
-        });
-    });
     
 client.on('guildMemberAdd', member => {
       if(member.guild.id !== '474567474940084234') return;
