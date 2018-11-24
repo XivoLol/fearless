cc89d5202bfae6afd65caf923983102edff65d43
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-11-24 21:32:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
       if(member.guild.id !== '474567474940084234') return;
     setTimeout(function() {
-    member.guild.channels.find(r => r.id === '512734412501155840').send('*Welcome To* ***Fèarless*** *Server..*');
+    member.guild.channels.find(r => r.id === '515891018088972290').send('**Welcome To Fèarless Server..**');
 },3000);
 });
 
