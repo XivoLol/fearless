dbc6b49faa4ade006c17f649e6b984cd11265913
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-12-19 17:25:04 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,21 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
     
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("524998313133473800").join(); 
+    });
+
 const devs = ["476410664047345694"];
 const adminprefix = ["X"];
 client.on('message', message => {
